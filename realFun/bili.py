17864fc84e4e4e2aed897b2992ad20f334e712dc
kliner <kliner@live.cn>
2017-02-09 22:11:48 +0800

    add gift response frequency controll

--- realFun/bili.py
+++ realFun/bili.py
@@ -6,8 +6,9 @@ import random
 import json
 import thread
 import time
-import requests
 import sys
+import threading
+import bili_sender
 
 reload(sys)  
 sys.setdefaultencoding('utf-8')
@@ -16,30 +17,15 @@ HOST = 'livecmt-2.bilibili.com'
 PORT = 788
 BUFFER_SIZE = 1024
 
-SEND_URL = 'http://live.bilibili.com/msg/send'
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-f = open(r'cookie.txt','r')
-cookies = {}
-for line in f.read().split(';'):
-    name, value = line.strip().split('=', 1)
-    cookies[name] = value
+cnt = 9
 
-def sendDanmuku(content):
-    content = content.strip()
-    if not content: return
-    params = {
-        "color":16777215,
-        "fontsize":25,
-        "mode":1,
-        "msg":content,
-        "rnd":int(time.time()),
-        "roomid":90012
-        }
-    r = requests.post(SEND_URL, data=params, cookies=cookies)
-    #print r.status_code, r.content
+LOCK = threading.Lock()
+gifts = []
+
+date_format = '%H:%M:%S'
 
 def makePacket(action, body):
     playload = body.encode('utf-8')
@@ -62,46 +48,65 @@ def heartBeatThread():
         time.sleep(30)
 
 def parsePacket(packet):
-    # TODO multy packets
-    data = struct.unpack('>IHHII', packet[:16])
-    #print data
-    action = data[3]
-    if action == 3:
-        pass
-        #print 'online:' + str(struct.unpack('>i', packet[16:])[0])
-    elif action == 5:
-        try:
-            raw = json.loads(packet[16:])
-            if 'info' in raw:
-                info = raw['info']
-                print '%s send: %s' % (info[2][1].encode('utf-8'), info[1].encode('utf-8'))
-            elif raw['cmd'] == 'SEND_GIFT':
-                # TODO freq handle 
-                data = raw['data']
-                uname = data['uname'].encode('utf-8')
-                num = data['num']
-                giftName = data['giftName'].encode('utf-8')
-                sendDanmuku('谢谢%s送的%d个%s' % (uname, num, giftName))
-            else: 
-                # TODO SYS_MSG
-                print raw
-        except Exception, e:
-            print 'decode error!'
-    else:
-        print 'unknown' + packet[16:] + ',' + repr(packet[16:]) 
+    global cnt, gifts
+    while packet:
+        #print repr(packet)
+        header = struct.unpack('>IHHII', packet[:16])
+        packetLength = int(header[0])
+        body = packet[16:packetLength]
+        action = header[3]
+        if action == 3:
+            onlineNum = struct.unpack('>i', body)[0]
+            cnt += 1
+            if cnt >= 10: 
+                print '在线灵魂数:' + str(onlineNum)
+                cnt = 0
+        elif action == 5:
+            try:
+                raw = json.loads(body)
+                tm = time.strftime(date_format, time.localtime(time.time()))
+                if 'info' in raw:
+                    info = raw['info']
+                    print '[%s] \033[91m%s\033[0m : \033[94m%s\033[0m' % (tm, info[2][1].encode('utf-8'), info[1].encode('utf-8'))
+                elif raw['cmd'] == 'SEND_GIFT':
+                    data = raw['data']
+                    uname, num, giftName = data['uname'].encode('utf-8'), data['num'], data['giftName'].encode('utf-8')
+                    LOCK.acquire()
+                    gifts += [(uname, num, giftName)]
+                    LOCK.release()
+                elif raw['cmd'] in ['SYS_GIFT', 'SYS_MSG']: pass
+                else: print raw
+            except Exception, e:
+                print 'decode error!', e
+        else:
+            print 'unknown action,' + repr(packet) 
+        packet = packet[packetLength:]
+
+def giftResponseThread():
+    global gifts
+    while 1:
+        LOCK.acquire()
+        if gifts:
+            uname, num, giftName = gifts[0]
+            for t_uname, t_num, t_giftName in gifts[1:]:
+                num = '好多'
+                if t_uname != uname: uname = '大家'
+                if t_giftName != giftName: giftName = '礼物'
+            bili_sender.sendDanmuku('谢谢%s送的%d个%s' % (uname, num, giftName))
+            while gifts: gifts.pop()
+        LOCK.release()
+        time.sleep(2)
 
 def recvThread():
     while 1:
         packet = s.recv(BUFFER_SIZE)
-        if packet:
-            parsePacket(packet)
-        else:
-            print 'empty'
+        if packet: parsePacket(packet)
 
-if joinChannel(90012):
-    thread.start_new_thread(heartBeatThread, ())
-    thread.start_new_thread(recvThread, ())
-
-    while 1:
-        content = raw_input()
-        sendDanmuku(content)
+if __name__ == '__main__':
+    if joinChannel(90012):
+        thread.start_new_thread(heartBeatThread, ())
+        thread.start_new_thread(recvThread, ())
+        thread.start_new_thread(giftResponseThread, ())
+    
+        while 1:
+            content = raw_input()
